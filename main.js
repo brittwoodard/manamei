098fceb64bcbf119dd8c72d2588764c53598289a
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2023-11-03 12:37:58 +0100

    slick + includes

---
+++ main.js
@@ -0,0 +1,12 @@
+// Slick
+$('.testimonial-container').slick({
+	autoplay: true, 
+	autoplaySpeed: 900, 
+	speed: 500, 
+	arrows: false, 
+	accessibility: true, 
+	dots: false, 
+	fade: true, 
+	infinite: true, 
+	pauseOnHover: false, 
+});
