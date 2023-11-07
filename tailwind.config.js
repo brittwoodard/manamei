2e7115aa5b8057b8707b8391db92dc0bb4b8a3da
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2023-11-07 16:50:49 +0100

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     theme: {
       fontFamily: {
         sans: ['Josefin Sans', 'sans-serif'],
-        serif: ['Italiana', 'serif'],
+        serif: ['modernline', 'serif'],
       },
       extend: {
         spacing: {
