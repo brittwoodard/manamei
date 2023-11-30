0ea34ec8d1f299081f6d90ac732aa128e39a4732
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2023-11-30 14:10:07 +0100

    foundations

--- main.js
+++ main.js
@@ -1,46 +1,58 @@
 // Slick
 $('.testimonial-container').slick({
-	autoplay: true, 
-	autoplaySpeed: 3000, 
-	speed: 500, 
-	arrows: false, 
-	accessibility: true, 
-	dots: true, 
-	fade: true, 
-	infinite: true, 
-	pauseOnHover: true, 
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500,
+    arrows: false,
+    accessibility: true,
+    dots: true,
+    fade: true,
+    infinite: true,
+    pauseOnHover: true,
 });
 
 $('.text-container').slick({
-	autoplay: true, 
-	speed: 1000, 
-	arrows: false, 
-	accessibility: true, 
-	dots: false, 
-	fade: true, 
-	infinite: true, 
-	pauseOnHover: false, 
+    autoplay: true,
+    speed: 1000,
+    arrows: false,
+    accessibility: true,
+    dots: false,
+    fade: true,
+    infinite: true,
+    pauseOnHover: false,
 });
 
 // Magnific
-$('.gallery').each(function() { 
-    $(this).find('a').magnificPopup( {
-        type: 'image',
-        gallery: {
-          enabled:true
+$(document).ready(function() {
+    $('.popup-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        titleSrc: function(item) {
+          return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
         }
+      }
     });
-});
+  });
+
 
 // Mobile Menu
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    $( document ).ready(function() {
-        $(".mobile-button").click(function(){
-          $(".mobile-button").toggleClass("active");
-          $(".mobile-menu").toggleClass("active");
+$(document).ready(function () {
+    $(document).ready(function () {
+        $(".mobile-button").click(function () {
+            $(".mobile-button").toggleClass("active");
+            $(".mobile-menu").toggleClass("active");
         });
-     });
+    });
 });
 
 // Read More
